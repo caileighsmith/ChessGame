2f5abce37f62a7676131badfeb7ea519ea4906e3
caileighsmith <caihidden@gmail.com>
2022-07-26 01:52:10 +0100

    major update

--- script.js
+++ script.js
@@ -1,143 +1,161 @@
-//notes : using initials for piece names, p for pawn as an example.
+//creating board.
+let boardContainer = document.getElementById('board-container')
 
-//board sets the dimensions for the array. Adding starting pos for pieces.
 let board = [
-    [ ['bRook'], ['bKnight'], ['bBishop'], ['bQueen'], ['bKing'], ['bBishop'], ['bKnight'], ['bRook']],
-    [ ['bPawn'], ['bPawn'], ['bPawn'], ['bPawn'], ['bPawn'], ['bPawn'], ['bPawn'], ['bPawn'] ],
-    [ [], [], [], [], [], [], [], [] ],
-    [ [], [], [], [], [], [], [], [] ],
-    [ [], [], [], [], [], [], [], [] ],
-    [ [], [], [], [], [], [], [], [] ],
-    [ ['wPawn'], ['wPawn'], ['wPawn'], ['wPawn'], ['wPawn'], ['wPawn'], ['wPawn'], ['wPawn'] ],
-    [ ['wRook'], ['wKnight'], ['wBishop'], ['wQueen'], ['wKing'], ['wBishop'], ['wKnight'], ['wRook'] ]
+    [['bR'],['bK'],['bB'],['bQ'],['bKn'],['bB'],['bK'],['bR']],
+    [['bP'],['bP'],['bP'],['bP'],['bP'],['bP'],['bP'],['bP']],
+    [[],[],[],[],[],[],[],[]],
+    [[],[],[],[],[],[],[],[]],
+    [[],[],[],[],[],[],[],[]],
+    [[],[],[],[],[],[],[],[]],
+    [['wP'],['wP'],['wP'],['wP'],['wP'],['wP'],['wP'],['wP']],
+    [['wR'],['wK'],['wB'],['wQ'],['wKn'],['wB'],['wK'],['wR']] 
 ]
 
-//board has not been created
-let boardCreated = false
-
-//We start a bPawn. index = [1][0] [2][0] [3][0]
-
-
-//index 1 and board.length-2 index(s) to access initial placement of line of pawns.
-
-
-//index 0 and board.length-1 index(s) to access 1st rows for white, and for black (king & queen row).
-console.log(board[0], board[board.length-1])
-
-//movement:
-let pawn = {
-    move1 : [+0][+1],
-    //only on first move:
-    move2 : [+0][+2]
-
-}
-
-
-let knight = {
-    move1 : [-1][+2],
-    move2 : [-2][+1],
-
-    move3 : [+1][+2],
-    move4 : [+2][+1],
-
-    move5 : [+2][-1],
-    move6 : [+1][-2],
-
-    move7 : [-1][-2],
-    move8 : [-2][-1]
-}
-
-
-//Pieces attatched to their ascii symbols.
+//building pieces
 let piece = {
     //black pieces with ascii
-    bPawn : '♟',
-    bKnight : '♞',
-    bBishop : '♝',
-    bRook : '♜',
-    bQueen : '♛',
-    bKing : '♚',
+    bP : '♟',
+    bK : '♞',
+    bB : '♝',
+    bR : '♜',
+    bQ : '♛',
+    bKn : '♚',
 
     //white pieces with ascii
-    wPawn : '♙',
-    wKnight : '♘',
-    wBishop : '♗',
-    wRook : '♖',
-    wQueen : '♕',
-    wKing : '♔'
+    wP : '♙',
+    wK : '♘',
+    wB : '♗',
+    wR : '♖',
+    wQ : '♕',
+    wKn : '♔'
 }
 
+//values of pieces
+let values = {
+    //black pieces with ascii
+    bP : 1,
+    bK : 3,
+    bB : 3,
+    bR : 5,
+    bQ : 9,
+    bKn : 0,
 
-//value of the pieces.
-let pieceValues = {
-    bPawn : '1',
-    bKnight : '3',
-    bBishop : '3',
-    bRook : '5',
-    bQueen : '9',
-    bKing : '0',
-
-    wPawn : '1',
-    wKnight : '3',
-    wBishop : '3',
-    wRook : '5',
-    wQueen : '9',
-    wKing : '0'
-
+    //white pieces with ascii
+    wP : 1,
+    wK : 3,
+    wB : 3,
+    wR : 5,
+    wQ : 9,
+    wKn : 0
 }
 
+//creating the board tiles
+function createBoard(){
+    for (let i = 0; i <board.length; i++){
+        //assinging a row variable.
+        let row = board[i]
 
+        //creating a container row div and adding it to DOM.
+        let rowDom = document.createElement('div')
+        rowDom.setAttribute('class', 'row n-'+i)
+        boardContainer.appendChild(rowDom)
+        console.log(i)
+        //looping through each row
+        for (let q = 0; q<row.length; q++){
+            
+            //creating the tile object, adding a class 'tile' and adding to row DOM.
+            let square = document.createElement('div')
 
-//Creating the chessboard using the 2 dimensional array created above.
-if (boardCreated == false){
-    for (i in board){
-        //current row
-        let current = board[i]
+            //checking if row index is odd
+            if (i % 2 != 0){
+                //if current square is on an even pos
+                if (q % 2 == 0){
+                    //then we put the white square on the DOM.
+                    square.setAttribute('class', 'square black')
+                    square.setAttribute('onclick', 'clickPiece(this)')
+                    square.setAttribute('id', String(i) + String(q))
+                    square.textContent = piece[row[q]]
+                    rowDom.appendChild(square)
+                }else{
+                    //else we put the black square on the DOM.
+                    square.textContent = piece[row[q]]
+                    square.setAttribute('class', 'square white')
+                    square.setAttribute('onclick', 'clickPiece(this)')
+                    square.setAttribute('id', String(i) + String(q))
+                    rowDom.appendChild(square)
 
-        //Creating a container div
-        let currentRow = document.createElement('div')
-        currentRow.setAttribute('id', 'row'+i)
-        document.getElementById('board-container').appendChild(currentRow)
+                }
 
-        //Looping through each row in array.
-        for (q in current){
-        
-            if (q % 2 == 0){
-                
-                //Creating a square object to push to DOM
-                var whiteSquare = document.createElement('div')
-                //++ attributes class 'white' and 'square'
-                whiteSquare.setAttribute('class', 'white square')
-                whiteSquare.setAttribute('id', 'squareId:'+i + ':'+ q)
-                whiteSquare.setAttribute('onclick', 'check(this)')
-
-                //Displaying the piece
-                if ( current[q].length != 0){
-                    console.log('piece: '+current[q])
-                    console.log(piece[current[q]])
-                    whiteSquare.textContent = piece[current[q]]
-                }else {console.log('Empty square')}
-                document.getElementById('row'+i).appendChild(whiteSquare);
+            //cehcking if row index is even,
             }else{
-                var blackSquare = document.createElement('div')
-                blackSquare.setAttribute('class', 'black square')
-                blackSquare.setAttribute('id', 'squareId:'+i + ':' +q)
-                blackSquare.setAttribute('onclick', 'check(this)')
-                //loging each piece
-                
+                if (q % 2 == 0){
+                    //then we put the white square on the DOM.
+                    square.setAttribute('class', 'square white')
+                    square.setAttribute('onclick', 'clickPiece(this)')
+                    square.setAttribute('id', String(i) + String(q))
+                    square.textContent = piece[row[q]]
+                    rowDom.appendChild(square)
+                }else{
+                    //else we put the black square on the DOM.
+                    square.setAttribute('class', 'square black')
+                    square.setAttribute('onclick', 'clickPiece(this)')
+                    square.setAttribute('id', String(i) + String(q))
+                    square.textContent = piece[row[q]]
+                    rowDom.appendChild(square)
 
-                //images
-                if ( current[q]){
-                    console.log('piece: '+current[q])
-                    console.log(piece[current[q]])
-                    blackSquare.textContent = piece[current[q]]
-                }else {console.log('Empty square')}
-                document.getElementById('row'+i).appendChild(blackSquare);
-            }
-
+                }
+            }              
         }
     }
 }
+
+
+function clickPiece(x){
+
+
+    //if the square is currently taken by a piece
+    if (x.innerHTML != ''){
+    
+        var pieceSelected = true
+        console.log('piece: '+x.innerHTML, 'taken: '+pieceSelected)
+        //assinging pos variable
+        let pos = x.id
+        console.log('Array position: '+pos)
+
+        //storing selected coords as selected piece
+        let selectedPiece = board[pos[0]][pos[1]]
+
+        //id is formated to YX. 0 is y, 1 is x. hence they are used to access co ords.
+        console.log(board[pos[0]][pos[1]])
+        
+        console.log('value: ' +values[board[pos[0]][pos[1]]])
+
+        let pieceInfo = document.getElementById('pieceInfo')
+
+        pieceInfo.innerHTML = selectedPiece + ' value: ' +values[board[pos[0]][pos[1]]] +'. array pos: '+pos 
+
+    //if the square is NOT taken by a piece
+    }else{
+        var pieceSelected = false
+        console.log('empty square')
+        console.log('piece: '+x.innerHTML, 'taken: '+pieceSelected)
+        //assinging pos variable
+        let pos = x.id
+        console.log('Array position: '+pos)
+
+
+    }
+   
+}
+
+//updating pieces on the board
+function updateBoard(){
+    
+
+
+}
+
 
 function findValues(){
     let whiteValues = 0;
@@ -148,10 +166,10 @@ function findValues(){
         for (let q = 0; q < thisRow.length; q++){
             let current = thisRow[q]
             if (current.length != 0 && current[0][0] == 'w'){
-                whiteValues = whiteValues + Number(pieceValues[current])
+                whiteValues = whiteValues + Number(values[current])
             }
             else if (current.length != 0 && current[0][0] == 'b'){
-                blackValues = blackValues + Number(pieceValues[current])
+                blackValues = blackValues + Number(values[current])
             }
             
             
@@ -171,25 +189,12 @@ function findValues(){
     
 }
 
-function drawBoard(){
-    for (let i = 0; i <drawBoard.length; i++){
-        let curRow = board[i]
-        for (let q = 0; q <curRow.length; q++){
-            console.log(curRow[q])
-        }
-    }
-}
 
-
-function check(x){
-    console.log(x.innerHTML)
-    
-    if (x.innerHTML == '♙'){
-        console.log('White pawn')
-        
-    }
-}
-
-//call this after each move is made. Checks scoring.
 findValues()
-drawBoard()
+
+
+updateBoard()
+
+
+
+createBoard()
